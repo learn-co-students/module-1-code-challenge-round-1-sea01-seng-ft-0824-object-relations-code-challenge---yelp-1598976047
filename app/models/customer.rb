502408ef8f81c631ce5e632f1c0348d54681b211
Cooliomoded <cooliomoded@gmail.com>
2020-09-01 11:04:49 -0700

    First Code Challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,45 @@
 class Customer
+
   attr_accessor :given_name, :family_name
+
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    all_restaurants = Review.all.select do
+      |review| review.customer == self.full_name
+    end
+    
+  end
+  #restaurants is unfinished... how to make it unique is blowing my mind right now...
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant.name, rating)
+  end
+
+  def num_reviews
+    number_of_reviews = Review.all.select { |review| review.customer == self.full_name }
+    number_of_reviews.length
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |customer| customer.full_name == name }
+  end
+
+  def self.find_all_by_given_name(name)
+    self.all.select { |customer| customer.given_name == name }
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,32 @@
 class Restaurant
+
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self.name }
+  end
+
+  def customers
+    self.reviews.select { |review| review.customer == self }
+  end
+  #unfinished, once again figuring out how to make the customers unique is blowing my mind...
+
+  def average_star_rating
+    sum = 0
+    star_ratings = self.reviews.collect { |review| review.rating }
+    star_ratings.each { |number| sum += number }
+    (sum.to_f / star_ratings.length).round(1)
   end
 
 end
